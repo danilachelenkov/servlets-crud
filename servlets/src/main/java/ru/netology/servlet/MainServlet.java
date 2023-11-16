992536470941b16b7bbbe9cf17019aaa1b9cd76f
DanilaChelenkov <chelenkov.dv@gmail.com>
2023-11-16 21:31:02 +0300

    added DI-Java task

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfiguration;
 import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
@@ -21,9 +23,12 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
+       /* final var repository = new PostRepository();
         final var service = new PostService(repository);
-        controller = new PostController(service);
+        controller = new PostController(service);*/
+
+        final var context = new AnnotationConfigApplicationContext(JavaConfiguration.class);
+        controller = context.getBean(PostController.class);
     }
 
     @Override
